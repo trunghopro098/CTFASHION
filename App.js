a36250105ae59ab0bb597c20daf9084854fdbc5a
trunghopro098 <trungho098@gmail.com>
2021-11-02 15:40:02 +0700

    create project react-native app

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = ()=>{
+
+return(
+  <View>
+    <Text>hello world</Text>
+  </View>
+)
+
+}
+
+export default App;
